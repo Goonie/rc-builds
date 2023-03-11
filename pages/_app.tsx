418f21703126b580335d4be8ebb4b550f4524618
Paul <github@goonie.nl>
2023-03-11 14:23:39 +0100

    Set up app structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-import '~/styles/globals.css';
+import '~/styles/app.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+
+        <title>RC Builds</title>
+        <meta name="description" content="Keep track of your RC builds" />
+
+        <link rel="icon" type="image/png" href="/favicon.png" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
